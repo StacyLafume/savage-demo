b6d17afeecd9bba18e8a63ec6038502cc2666188
Resilient Coders 3 <resilientcoders3@Resilients-MacBook-Pro-2.local>
2019-07-28 14:55:15 -0400

    Savage demo

--- server.js
+++ server.js
@@ -54,6 +54,23 @@ app.put('/messages', (req, res) => {
   })
 })
 
+
+app.put('/decrease', (req, res) => {
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbDown - 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+
 app.delete('/messages', (req, res) => {
   db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
     if (err) return res.send(500, err)
